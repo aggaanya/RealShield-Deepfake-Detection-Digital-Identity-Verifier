9db4a499a3cd7aead5ffcae5ed92e3387692b4f4
Aanya Aggarwal <aggaanya15@gmail.com>
2026-01-16 12:06:46 +0530

    changes in metadata

--- backend/src/main/java/com/realshield/platform/TestController.java
+++ backend/src/main/java/com/realshield/platform/TestController.java
@@ -1,10 +1,14 @@
 package com.realshield.platform;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequestMapping("/api/test")
 public class TestController {
 
     @GetMapping("/test")
